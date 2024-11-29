8e949c22218805c287389c9f0ed9a6a07f6b5eb0
geniusxxx <1632044688@qq.com>
2024-11-29 09:34:16 +0800

    fix(open_clip/src/open_clip/timm_model.py): fix the bug of proj_dim (#84)

--- src/open_clip/timm_model.py
+++ src/open_clip/timm_model.py
@@ -33,7 +33,7 @@ sys.path.append(project_root)
 
 # 导入自定义 FastViT 实现
 from custom_timm.models.fastvit import *  # 这会注册自定义模型到 TIMM
-from custom_timm.models.repvit import *  # 这会注册自定义模型到 TIMM
+# from custom_timm.models.maxxvit import *  # 这会注册自定义模型到 TIMM
 
 class TimmModel(nn.Module):
     """ timm model adapter
@@ -83,8 +83,6 @@ class TimmModel(nn.Module):
         else:
             self.trunk = timm.create_model(
                 model_name,
-                num_classes=proj_dim,
-                global_pool=pool,
                 pretrained=pretrained,
                 **timm_kwargs,
             )
